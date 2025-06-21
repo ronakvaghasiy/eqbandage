aae4c99c5e76e10dfa6bad58aef2831c2a981315
Ronak Vaghasiya <87721124+ronakvaghasiy@users.noreply.github.com>
2025-06-21 18:30:35 +0530

    Add files via upload

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testMatch: ['**/__tests__/**/*.(test|spec).(ts|tsx)'],
+};
